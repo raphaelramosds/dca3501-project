ed8e8b86ac272b7b772f183abb1cd706671335af
vitorgabriel27 <vitor.alves@geowellex.com>
2025-06-09 21:43:55 -0300

    feat(tabs): create tabs

---
+++ streamlit/tabs/reports.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def render_reports():
+    st.subheader("Relatórios")
+    st.write("Aqui vão os relatórios detalhados.")
---
+++ streamlit/tabs/settings.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def render_settings():
+    st.subheader("Configurações")
+    st.write("Aqui vão as configurações do sistema.")
