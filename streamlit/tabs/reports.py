cb14ef6c779e9506a404bc9c81d412a9d5b99981
vitorgabriel27 <vitor.alves@geowellex.com>
2025-06-12 21:53:33 -0300

    chore: remove unused files

--- streamlit/tabs/reports.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-import components.series_aqi_polutants as series_aqi_polutants
-
-def render_reports():
-    series_aqi_polutants.render_series_polutants()
--- streamlit/tabs/settings.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def render_settings():
-    st.subheader("Configurações")
-    st.write("Aqui vão as configurações do sistema.")
